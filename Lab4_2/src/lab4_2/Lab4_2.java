f116a31ccf766818b761486dee1ee32b5abed237
Faheem Hassan Zunjani <faheemzunjani@gmail.com>
2016-09-22 15:54:08 +0530

    Add Lab4_2

---
+++ Lab4_2/src/lab4_2/Lab4_2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4_2;
+
+public class Lab4_2
+{
+	public static void main(String args[])
+	{		
+		
+		System.out.println("Name : " + args[0]);
+		System.out.println("Institute : " + args[1]);
+		System.out.println("Email : " + args[2]);
+	}
+}
